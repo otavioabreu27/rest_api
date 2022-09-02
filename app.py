8c966651c5a266f86f63e7831a33c8351a81f2c2
otavioabreu27 <otavio.abreu96@gmail.com>
2022-09-02 20:56:59 -0300

    Integração com o Alchemy

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from resources.hotel import Hoteis, Hotel
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #instanciando a api do restful
 api = Api(app)
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 #adicionanco como recurso da api restful as classes importadas do arquivo hotel
 api.add_resource(Hoteis, '/hoteis') 
@@ -15,5 +21,7 @@ api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 #roda o codigo
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
 
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,33 +1,9 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-    'hotel_id': 'alpha',
-    'nome': 'Alpha Hotel',
-    'estrelas': 2.3,
-    'diaria': 418.34,
-    'cidade': 'Guarulhos'
-    },
-    {
-    'hotel_id': 'bravo',
-    'nome': 'Bravo Hotel',
-    'estrelas': 2.0,
-    'diaria': 398.00,
-    'cidade': 'Belo Horizonte'
-    },
-    {
-    'hotel_id': 'charlie',
-    'nome': 'Charlie Hotel',
-    'estrelas': 1.9,
-    'diaria': 268.00,
-    'cidade': 'Salvador'
-    }
-]
-
 class Hoteis(Resource):
     def get(self):
-       return {'hoteis': hoteis}
+       return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
     #instancia da função RequestParser() que facilita o manejo de arquivos json
@@ -37,43 +13,38 @@ class Hotel(Resource):
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def find_hotel(hotel_id):
-        #percorre o dicionario
-        for hotel in hoteis:
-            #se o dicionario tiver um par chave valor com a chave id igual
-            #ao passado pelo usuario ele retorna a inserção do dicionario
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
     def get(self, hotel_id):
         #função que verifica se ele existe
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         #caso hotel não seja nulo
         if hotel:
-            return hotel
+            return hotel.json()
         #senão
         return {'message': 'Hotel not found.'}, 404 #not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return{"message": "Hotel id '{}' already exists".format(hotel_id)}, 400 #bad requiest
+
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201 # created criado
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201 # created criado
 
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel ['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted.'}
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            hotel.delete_hotel()
+            return {'message': 'Hotel deleted.'}
+        return {'message': 'Hotel not found.'}, 404
